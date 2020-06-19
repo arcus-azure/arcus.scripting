60a825c2b54ca4e2b9e93efe4bc982caba218d9d
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2020-06-19 07:40:16 +0200

    Update Set-AzKeyVaultSecretFromFile.ps1

--- src/Arcus.Scripting.KeyVault/Scripts/Set-AzKeyVaultSecretFromFile.ps1
+++ src/Arcus.Scripting.KeyVault/Scripts/Set-AzKeyVaultSecretFromFile.ps1
@@ -1,16 +1,16 @@
 # Use this script to upload a certificate as plain text (multiline-support) into Azure KeyVault
 
 param (
-    [string][Parameter(Mandatory=$true)] $filePath = $(throw "The path to the file is required."),
-    [string][Parameter(Mandatory=$true)] $keyVaultName = $(throw "The path to the file is required."),
-    [string][Parameter(Mandatory=$true)] $secretName = $(throw "The path to the file is required."),
-    [bool][parameter(Mandatory = $false)] $loggedIn = $true,
-    [string][parameter(Mandatory = $false)] $subscriptionId = ""
+    [string][Parameter(Mandatory=$true)] $FilePath = $(throw "The path to the file is required."),
+    [string][Parameter(Mandatory=$true)] $KeyVaultName = $(throw "The path to the file is required."),
+    [string][Parameter(Mandatory=$true)] $SecretName = $(throw "The path to the file is required."),
+    [bool][parameter(Mandatory = $false)] $LoggedIn = $true,
+    [string][parameter(Mandatory = $false)] $SubscriptionId = ""
 )
 
 # Perform the deployment based on the provided ARM-template and parameter file, if provided.
 Write-Host("Creating KeyVault secret...")
 
-Set-AzKeyVaultSecret -VaultName $keyVaultName -SecretName $secretName -SecretValue (ConvertTo-SecureString (Get-Content $filePath -Raw) -force -AsPlainText ) -ErrorAction Stop
+Set-AzKeyVaultSecret -VaultName $KeyVaultName -SecretName $SecretName -SecretValue (ConvertTo-SecureString (Get-Content $FilePath -Raw) -force -AsPlainText ) -ErrorAction Stop
 
-Write-Host("Secret '$secretName' has been created.")
+Write-Host("Secret '$SecretName' has been created.")
