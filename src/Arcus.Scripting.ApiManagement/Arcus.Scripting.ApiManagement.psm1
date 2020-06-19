783ee314627979dbed684fb1e567487ff1e5c1fe
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2020-06-19 13:19:28 +0200

    Feature - create new API operation on API Management service script

--- src/Arcus.Scripting.ApiManagement/Arcus.Scripting.ApiManagement.psm1
+++ src/Arcus.Scripting.ApiManagement/Arcus.Scripting.ApiManagement.psm1
@@ -1,6 +1,50 @@
 <#
-	My Function
-#>
-function Get-Function {
+ .Synopsis
+  Create an operation on an API in Azure API Management.
 
+ .Description
+  Create an operation on an existing API in Azure API Management.
+
+ .Parameter ServiceName
+  The name of the API Management service located in Azure.
+
+ .Parameter ResourceGroupName
+  The resource group containing the API Management service.
+
+ .Parameter ApiId
+  The ID to identify the API running in API Management.
+
+ .Parameter OperationId
+  The ID to identify the to-be-created operation on the API.
+
+ .Parameter Method
+  The method of the to-be-created operation on the API.
+
+ .Parameter UrlTemplate
+  The template of the URL of the to-be-created operation on the API.
+
+ .Parameter OperationName
+  The optional descriptive name to give to the to-be-created operation on the API.
+
+ .Parameter Description
+  The optional explaination to describe the to-be-created operation on the API.
+
+ .Parameter PolicyFilePath
+  The path to the file containing the optional policy of the to-be-created operation on the API.
+#>
+function Create-AzApiManagementApiOperation {
+	param(
+        [string][Parameter(Mandatory = $true)] $ResourceGroup = $(throw "Resource group is required"),
+        [string][Parameter(Mandatory = $true)] $ServiceName = $(throw "API management service name is required"),
+        [string][Parameter(Mandatory = $true)] $ApiId = $(throw "API ID is required"),
+        [string][Parameter(Mandatory = $true)] $OperationId = $(throw "Operation ID is required"),
+        [string][Parameter(Mandatory = $true)] $Method = $(throw "Method is required"),
+        [string][Parameter(Mandatory = $true)] $UrlTemplate = $(throw "URL template is required"),
+        [string][Parameter(Mandatory = $false)] $OperationName = $OperationId,
+        [string][Parameter(Mandatory = $false)] $Description = "",
+        [string][Parameter(Mandatory = $false)] $PolicyFilePath = ""
+	)
+	. $PSScriptRoot\Scripts\Create-AzApiManagementApiOperation.ps1 -ResourceGroup $ResourceGroup -ServiceName $ServiceName -ApiId $ApiId -OperationId $OperationId -Method $Method -UrlTemplate $UrlTemplate -OperationName $OperationName -Description $Description -PolicyFilePath $PolicyFilePath
 }
+
+Export-ModuleMember -Function Create-AzApiManagementApiOperation
---
+++ src/Arcus.Scripting.Tests.Unit/Arcus.Scripting.ApiManagement.tests.ps1
@@ -0,0 +1,78 @@
+﻿Import-Module -Name $PSScriptRoot\..\Arcus.Scripting.ApiManagement -ErrorAction Stop
+
+Describe "Arcus" {
+	Context "ApiManagement" {
+		InModuleScope Arcus.Scripting.ApiMangement {
+			It "Calls new operation on API Management operation w/o policy" {
+				# Arrange
+				$resourceGroup = "shopping"
+				$serviceName = "shopping-API-management"
+				$apiId = "shopping-API"
+				$operationId = "orders"
+				$method = "POST"
+				$urlTemplate = "https://{host}.com/{path}{query}"
+				$context = "API-management-context"
+
+				Mock New-AzApiManagementContext {
+					$ResourceGroup | Should -Be $resourceGroup
+					$ServiceName | Should -Be $serviceName
+					return $context } -Verifiable
+				Mock New-AzApiManagementOperation { 
+					$Context | Should -Be $context
+					$ApiId | Should -Be $apiId
+					$OperationId | Should -Be $operationId
+					$Method | Should -Be $method
+					$UrlTemplate | Should -Be $urlTemplate } -Verifiable
+				Mock Set-AzApiManagementPolicy { }
+
+				# Act
+				Create-AzApiManagementApiOperation -ResourceGroup $resourceGroup -ServiceName $serviceName -ApiId $apiId -OperationId $operationId -Method $method -UrlTemplate $urlTemplate
+
+				# Assert
+				Assert-VerifiableMock
+				Assert-MockCalled New-AzApiManagementContext -Times 1
+				Assert-MockCalled New-AzApiManagementOperation -Times 1
+				Assert-MockCalled Set-AzApiManagementPolicy -Times 0
+			}
+			It "Calls new operation on API management operation w/ policy" {
+				# Arrange
+				$resourceGroup = "shopping"
+				$serviceName = "shopping-API-management"
+				$apiId = "shopping-API"
+				$operationId = "orders"
+				$method = "POST"
+				$urlTemplate = "https://{host}.com/{path}{query}"
+				$context = "API-management-context"
+				$operationName = "POSTing orders"
+				$description = "API that can process posted orders"
+				$policyFilePath = "/file-path/operation-policy"
+
+				Mock New-AzApiManagementContext {
+					$ResourceGroup | Should -Be $resourceGroup
+					$ServiceName | Should -Be $serviceName
+					return $context } -Verifiable
+				Mock New-AzApiManagementOperation { 
+					$Context | Should -Be $context
+					$ApiId | Should -Be $apiId
+					$OperationId | Should -Be $operationId
+					$Method | Should -Be $method
+					$UrlTemplate | Should -Be $urlTemplate
+					$Description | Should -Be $description } -Verifiable
+				Mock Set-AzApiManagementPolicy {
+					$Context | Should -Be $context
+					$ApiId | Should -Be $apiId
+					$OperationId | Should -Be $operationId
+					$PolicyFilePath | Should -Be $policyFilePath } -Verifiable
+
+				# Act
+				Create-AzApiManagementApiOperation -ResourceGroup $resourceGroup -ServiceName $serviceName -ApiId $apiId -OperationId $operationId -Method $method -UrlTemplate $urlTemplate -OperationName $operationName -Description $Description -PolicyFilePath $policyFilePath
+
+				# Assert
+				Assert-VerifiableMock
+				Assert-MockCalled New-AzApiManagementContext -Times 1
+				Assert-MockCalled New-AzApiManagementOperation -Times 1
+				Assert-MockCalled Set-AzApiManagementPolicy -Times 1
+			}
+		}
+	}
+}
