d934735548f1e6da04ab95c6a895c4e2b9937a87
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2020-06-21 23:16:41 +0200

    Update Set-AzKeyVaultSecretFromFile.ps1

--- src/Arcus.Scripting.KeyVault/Scripts/Set-AzKeyVaultSecretFromFile.ps1
+++ src/Arcus.Scripting.KeyVault/Scripts/Set-AzKeyVaultSecretFromFile.ps1
@@ -3,13 +3,19 @@
 param (
     [string][Parameter(Mandatory=$true)] $FilePath = $(throw "The path to the file is required."),
     [string][Parameter(Mandatory=$true)] $KeyVaultName = $(throw "The path to the file is required."),
-    [string][Parameter(Mandatory=$true)] $SecretName = $(throw "The path to the file is required.")
+    [string][Parameter(Mandatory=$true)] $SecretName = $(throw "The path to the file is required."),
+    [System.DateTime][Parameter(Mandatory=$false)] $Expires
 )
 
 Write-Host "Creating KeyVault secret..."
 
 $rawContent = Get-Content $FilePath -Raw
-$secretValue = ConvertTo-SecureString $rawContent -Force -AsPlainText
-Set-AzKeyVaultSecret -VaultName $KeyVaultName -SecretName $SecretName -SecretValue $secretValue -ErrorAction Stop
+$secretValue = ConvertTo-SecureString $rawContent -Force -AsPlainTex
+
+if ($Expires -ne $null) {
+    Set-AzKeyVaultSecret -VaultName $KeyVaultName -SecretName $SecretName -SecretValue $secretValue -Expires $Expires -ErrorAction Stop
+} else {
+    Set-AzKeyVaultSecret -VaultName $KeyVaultName -SecretName $SecretName -SecretValue $secretValue -ErrorAction Stop
+}
 
 Write-Host "Secret '$SecretName' has been created."
