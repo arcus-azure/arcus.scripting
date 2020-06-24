08fd4358e80089b5b858179ae907146104d5f18d
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2020-06-24 09:35:31 +0200

    pr-fix: remove API id ref

--- src/Arcus.Scripting.ApiManagement/Scripts/Import-AzApiManagementProductPolicy.ps1
+++ src/Arcus.Scripting.ApiManagement/Scripts/Import-AzApiManagementProductPolicy.ps1
@@ -10,7 +10,7 @@ $apimContext = New-AzApiManagementContext -ResourceGroupName $ResourceGroup -Ser
 
 # Check if an operationId has been specified, it not - import the base policy
 Write-Host "Updating policy of product '$ProductId'"
-$result = Set-AzApiManagementPolicy -Context $apimContext -ProductId $ApiId -PolicyFilePath $PolicyFilePath
+$result = Set-AzApiManagementPolicy -Context $apimContext -ProductId $ProductId -PolicyFilePath $PolicyFilePath
 if ($result) {
     Write-Host "Successfully updated the product policy"
 } else {
