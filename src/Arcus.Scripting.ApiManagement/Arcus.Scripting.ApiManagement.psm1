6d8577ba752611759c27a1333c0e2c7eba42e4b3
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2020-06-25 09:32:37 +0200

    Update src/Arcus.Scripting.ApiManagement/Arcus.Scripting.ApiManagement.psm1
    Co-authored-by: Maxim Braekman <maxim.braekman@hotmail.com>

--- src/Arcus.Scripting.ApiManagement/Arcus.Scripting.ApiManagement.psm1
+++ src/Arcus.Scripting.ApiManagement/Arcus.Scripting.ApiManagement.psm1
@@ -54,7 +54,7 @@ Export-ModuleMember -Function Create-AzApiManagementApiOperation
   Import a policy to a product in Azure API Management.
 
  .Description
-  Import a policy to an API scope in a Azure API Management service.
+  Import a policy to a product in Azure API Management.
 
  .Parameter ResourceGroupName
   The resource group containing the API Management service.
