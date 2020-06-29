4a338b21de9ef94f2fc9e3e6c245d6ea7c11e25e
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2020-06-29 08:23:13 +0200

    Update Arcus.Scripting.ApiManagement.tests.ps1

--- src/Arcus.Scripting.Tests.Unit/Arcus.Scripting.ApiManagement.tests.ps1
+++ src/Arcus.Scripting.Tests.Unit/Arcus.Scripting.ApiManagement.tests.ps1
@@ -98,6 +98,32 @@ Describe "Arcus" {
 				# Assert
 				Assert-VerifiableMock
 			}
+                        It "Importing policy API fails API Management policy on operation" {
+			    # Arrange
+			    $resourceGroup = "shopping"
+			    $serviceName = "shopping-API-management"
+			    $apiId = "shopping-API"
+			    $policyFilePath = "/file-path/operation-policy"
+			    $context = New-Object -TypeName Microsoft.Azure.Commands.ApiManagement.ServiceManagement.Models.PsApiManagementContext
+
+                            Mock New-AzApiManagementContext {
+			        $ResourceGroup | Should -Be $resourceGroup
+				$ServiceName | Should -Be $serviceName
+				return $context } -Verifiable
+			    Mock Set-AzApiManagementPolicy {
+			        $Context | Should -Be $context
+				$ApiId | Should -Be $apiId
+				$OperationId | Should -Be $operationId
+				$PolicyFilePath | Should -Be $policyFilePath
+				return $false } -Verifiable
+
+			    # Act
+			    { Import-AzApiManagementApiPolicy -ResourceGroup $resourceGroup -ServiceName $serviceName -ApiId $apiId -PolicyFilePath $policyFilePath } |
+			        Should -Throw
+
+			    # Assert
+			    Assert-VerifiableMock
+			}
 		}
 	}
 }
