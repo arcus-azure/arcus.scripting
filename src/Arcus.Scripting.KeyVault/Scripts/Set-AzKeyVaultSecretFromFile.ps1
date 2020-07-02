1fdb6f777e318b7a2be617e25a2d052cba9d518e
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2020-07-02 07:20:53 +0200

    Update Set-AzKeyVaultSecretFromFile.ps1

--- src/Arcus.Scripting.KeyVault/Scripts/Set-AzKeyVaultSecretFromFile.ps1
+++ src/Arcus.Scripting.KeyVault/Scripts/Set-AzKeyVaultSecretFromFile.ps1
@@ -1,11 +1,15 @@
 # Use this script to upload a certificate as plain text (multiline-support) into Azure Key Vault.
 
 param (
-    [string][Parameter(Mandatory=$true)] $FilePath = $(throw "The path to the file is required."),
     [string][Parameter(Mandatory=$true)] $KeyVaultName = $(throw "The path to the file is required."),
     [string][Parameter(Mandatory=$true)] $SecretName = $(throw "The path to the file is required."),
+    [string][Parameter(Mandatory=$true)] $FilePath = $(throw "The path to the file is required."),
     [System.Nullable[System.DateTime]][Parameter(Mandatory=$false)] $Expires
 )
+
+if (-not Test-Path $FilePath) {
+    Write-Error "No file could containing the secret certificate at '$FilePath'"
+}
 
 Write-Host "Creating KeyVault secret..."
 
