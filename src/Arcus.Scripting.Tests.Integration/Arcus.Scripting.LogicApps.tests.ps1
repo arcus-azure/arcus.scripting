f719d7fcb306f8bbeadf17595b9cbb9c3a0ee090
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2020-09-14 12:08:34 +0200

    pr-fix: use correct condition

--- src/Arcus.Scripting.Tests.Integration/Arcus.Scripting.LogicApps.tests.ps1
+++ src/Arcus.Scripting.Tests.Integration/Arcus.Scripting.LogicApps.tests.ps1
@@ -100,7 +100,7 @@ Describe "Arcus" {
                 $i = 0
                 Mock Get-AzLogicAppRunHistory {
                     $script:i++
-                    if ($script:i -ge 2) {
+                    if ($script:i -gt 2) {
                         Write-Host "Returning empty (no running, no waiting) runs"
                         return @()
                     } else {
