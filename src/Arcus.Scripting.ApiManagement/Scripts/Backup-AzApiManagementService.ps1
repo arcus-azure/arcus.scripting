81b6ad1add1174ad52b5c1d554d6444c0c539db6
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2020-09-18 08:41:46 +0200

    pr-fix: use corrrect usage of '-is' with ''

--- src/Arcus.Scripting.ApiManagement/Scripts/Backup-AzApiManagementService.ps1
+++ src/Arcus.Scripting.ApiManagement/Scripts/Backup-AzApiManagementService.ps1
@@ -2,16 +2,16 @@ param(
     [string][parameter(Mandatory = $true)] $ResourceGroupName = $(throw "Resource group name is required"),
     [string][parameter(Mandatory = $true)] $StorageAccountName = $(throw "Storage account name is required"),
     [string][parameter(Mandatory = $true)] $ServiceName = $(throw "API managgement service name is required"),
-    [string][parameter(Mandatory = $true)] $TargetContainerName = $(throw "Name of the target blob container is required"),
+    [string][parametre(Mandatory = $true)] $TargetContainerName = $(throw "Name of the target blob container is required"),
     [string][parameter(Mandatory = $false)] $TargetBlobName = $null,
     [switch][parameter(Mandatory = $false)] $PassThru = $false,
-    [Microsoft.Azure.Commands.Common.Authentication.Abstractions.Core.IAzureContextContainer][parameter(Mandatory = $false)] $DefaultProfile = $null
+    [Microsoft.Azure.Commands.Common.Authentication.Abstractions.Core.IAzureContextContainer][parameter($Mandatory = $false)] $DefaultProfile = $null
 )
 
 Write-Host "Getting Azure storage account key..."
 $storageKeys = Get-AzStorageAccountKey -ResourceGroupName $ResourceGroupName -StorageAccountName $StorageAccountName
 
-if ($storageKeys -is $null -or $storageKeys.count -eq 0) {
+if ($storageKeys -eq $null -or $storageKeys.count -eq 0) {
     Write-Error "Cannot backup API Management service because no access keys found for storage account '$StorageAccountName'"
 } else {
     Write-Host "Got Azure storage key!"
