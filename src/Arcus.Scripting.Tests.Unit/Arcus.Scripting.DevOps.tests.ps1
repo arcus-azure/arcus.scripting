c20fb311feadca9b490bafe978eb0889080399a4
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2020-10-12 09:18:45 +0200

    pr-fix: use body assertion

--- src/Arcus.Scripting.Tests.Unit/Arcus.Scripting.DevOps.tests.ps1
+++ src/Arcus.Scripting.Tests.Unit/Arcus.Scripting.DevOps.tests.ps1
@@ -38,6 +38,39 @@ Describe "Arcus" {
                 # Assert
                 Assert-VerifiableMock
             }
+            It "Setting DevOps variable group from ARM outputs should send info to DevOps project and update current job pipevariables" {
+                # Arrange
+                $variableGroupName = "some-variable-group-name"
+                $env:ArmOutputs = "{ ""$variableGroupName"": [ { ""Name"": ""my-variable"", ""Value"": { ""value"": ""my-value"" } } ] }"
+                $env:SYSTEM_ACCESSTOKEN = "something to fill"
+
+                $variableName = "some-id"
+                $setCurrentJobVariable = $false
+
+                Mock Invoke-RestMethod {
+                    if ($Method -eq "Post" -or $Method -eq "Put") {
+                        $Uri | Should -BeLike "*$variableGroupName*"
+                        $Body | Should -BeLike "*$variableName*"
+                        return $null
+                    } else {
+                        $Uri | Should -BeLike "*$variableGroupName*"
+                        return [pscustomobject]@{ value = @( [pscustomobject]@{ id = $variableName; variables = [pscustomobject]@{} } ) }
+                    }
+                } -Verifiable
+                Mock Write-Host {
+                    if ($Object -like "##vso*") {
+                        $Object | Should -Be "##vso[task.setvariable variable=my-variable]my-value"
+                        $setCurrentJobVariable = $true
+                    }
+                } -Verifiable
+
+                # Act
+                Set-AzDevOpsArmOutputsToVariableGroup -VariableGroupName $variableGroupName -UpdateVariablesForCurrentJob
+
+                # Assert
+                Assert-VerifiableMock
+                $setCurrentJobVariable | Should -Be $true
+            }
         }
     }
 }
