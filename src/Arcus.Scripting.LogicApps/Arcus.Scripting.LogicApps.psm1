2f9b9d127f7e2e465c9fb13f35ceafdf2f45da8c
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2020-10-15 07:51:57 +0200

    Update src/Arcus.Scripting.LogicApps/Arcus.Scripting.LogicApps.psm1
    Co-authored-by: Maxim Braekman <maxim.braekman@hotmail.com>

--- src/Arcus.Scripting.LogicApps/Arcus.Scripting.LogicApps.psm1
+++ src/Arcus.Scripting.LogicApps/Arcus.Scripting.LogicApps.psm1
@@ -3,7 +3,7 @@
   Disable all specified Logic Apps described in the order control JSON file.
   
  .Description
-  Disables all specified Logic Apps in a specific order. The Logic Apps to be disabled and the order in which this will be done, will be defined in the deploy-OrderControl.json-file.
+  Disables all specified Logic Apps in a specific order. The Logic Apps to be disabled and the order in which this will be done, will be defined in the configuration file (e.g. deploy-orderControl.json).
 
  .Parameter DeployFileName
   If your solution consists of multiple interfaces, you can specify the flow-specific name of the orderControl-file, if not, the script will look for a file named 'deploy-orderControl.json' by default.
