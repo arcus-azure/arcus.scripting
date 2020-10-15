4d9c1b7c2f9bf260a4c300b7d20b7acc125bfdaa
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2020-10-15 07:52:15 +0200

    Update src/Arcus.Scripting.LogicApps/Arcus.Scripting.LogicApps.psm1
    Co-authored-by: Maxim Braekman <maxim.braekman@hotmail.com>

--- src/Arcus.Scripting.LogicApps/Arcus.Scripting.LogicApps.psm1
+++ src/Arcus.Scripting.LogicApps/Arcus.Scripting.LogicApps.psm1
@@ -5,11 +5,11 @@
  .Description
   Disables all specified Logic Apps in a specific order. The Logic Apps to be disabled and the order in which this will be done, will be defined in the configuration file (e.g. deploy-orderControl.json).
 
- .Parameter DeployFileName
-  If your solution consists of multiple interfaces, you can specify the flow-specific name of the orderControl-file, if not, the script will look for a file named 'deploy-orderControl.json' by default.
-
  .Parameter ResourceGroupName
   The resource group containing the Azure Logic Apps.
+  
+ .Parameter DeployFileName
+  If your solution consists of multiple interfaces, you can specify the flow-specific name of the configuration file, if not, the script will look for a file named 'deploy-orderControl.json' by default.
 #>
 function Disable-AzLogicAppsFromConfig {
     param(
