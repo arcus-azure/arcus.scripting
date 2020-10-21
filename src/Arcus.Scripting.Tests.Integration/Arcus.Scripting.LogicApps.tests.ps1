b61406676946d67a55baf77f228892cc9bc72e23
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2020-10-21 07:17:53 +0200

    Feature - disables Azure Logic Apps from configuration file (#89)
    * Feature - disables Azure Logic Apps from configuration file
    
    * pr-add: index link to Azure Logic Apps feature docs
    
    * pr-fix: add integration tests to CI & release build
    
    * pr-fix: add integration tests to dependency of actual release task
    
    * pr-fix: add missing curly bracket
    
    * pr-fix: correct value expression in tests
    
    * pr-fix: correct value expression in tests
    
    * pr-fix: add dashes to assertion
    
    * pr-fix: add 'Az' using
    
    * pr-fix: use correct script file name
    
    * pr-fix: use correct Pester assertion sub-command
    
    * pr-fix: use correct argument names
    
    * pr-fix: remove empty lines from inline value file
    
    * pr-fix: use correct deploy file name argument name
    
    * pr-fix: correct naming
    
    * pr-sug: correct reversing
    
    * pr-fix: use 0 as stop result
    
    * pr-fix: correct test assertions
    
    * pr-sug: update with correct condition
    
    * pr-test: use sep test file
    
    * pr-test: add test logging
    
    * pr-fix: more correct creating custom ps objects
    
    * pr-fix: use broader var scope
    
    * pr-fix: use correct var scope
    
    * pr-fix: use correct condition
    
    * pr-sug: use correct call count in assertion
    
    * Update Arcus.Scripting.LogicApps.pssproj
    
    * Update src/Arcus.Scripting.LogicApps/Arcus.Scripting.LogicApps.psm1
    
    Co-authored-by: Maxim Braekman <maxim.braekman@hotmail.com>
    
    * Update src/Arcus.Scripting.LogicApps/Arcus.Scripting.LogicApps.psm1
    
    Co-authored-by: Maxim Braekman <maxim.braekman@hotmail.com>
    
    * Update docs/preview/features/powershell/azure-logic-apps.md
    
    Co-authored-by: Maxim Braekman <maxim.braekman@hotmail.com>
    
    * Update docs/preview/features/powershell/azure-logic-apps.md
    
    Co-authored-by: Maxim Braekman <maxim.braekman@hotmail.com>
    
    * Update docs/preview/features/powershell/azure-logic-apps.md
    
    Co-authored-by: Maxim Braekman <maxim.braekman@hotmail.com>
    
    * Update docs/preview/features/powershell/azure-logic-apps.md
    
    Co-authored-by: Maxim Braekman <maxim.braekman@hotmail.com>
    
    * Update docs/preview/features/powershell/azure-logic-apps.md
    
    Co-authored-by: Maxim Braekman <maxim.braekman@hotmail.com>
    
    * Update docs/preview/features/powershell/azure-logic-apps.md
    
    Co-authored-by: Maxim Braekman <maxim.braekman@hotmail.com>
    
    * Update docs/preview/features/powershell/azure-logic-apps.md
    
    Co-authored-by: Maxim Braekman <maxim.braekman@hotmail.com>
    
    * Update azure-logic-apps.md
    
    * Update Arcus.Scripting.LogicApps.psm1
    
    * Update Disable-AzLogicAppsFromConfig.ps1
    
    * Update Arcus.Scripting.LogicApps.psm1
    
    * Update azure-logic-apps.md
    
    Co-authored-by: Maxim Braekman <maxim.braekman@hotmail.com>

---
+++ src/Arcus.Scripting.Tests.Integration/Arcus.Scripting.LogicApps.tests.ps1
@@ -0,0 +1,125 @@
+using module Az
+Import-Module -Name $PSScriptRoot\..\Arcus.Scripting.LogicApps -ErrorAction Stop
+
+Describe "Arcus" {
+    Context "LogicApps" {
+        InModuleScope Arcus.Scripting.LogicApps {
+            It "Doesn't disable anything when the checkType is not recognized" {
+                # Arrange
+                Mock Get-AzLogicAppRunHistory {}
+
+                # Act
+                Disable-AzLogicAppsFromConfig -DeployFileName "$PSScriptRoot\Files\deploy-orderControl-unknownCheckType.json" -ResourceGroup "ignored-resource-group"
+
+                # Assert
+                Assert-MockCalled Get-AzLogicAppRunHistory -Times 0
+            }
+            It "Doesn't disable anything when the stopType is not recognized" {
+                # Arrange
+                Mock Get-AzLogicAppRunHistory {}
+                Mock Set-AzLogicApp {}
+
+                # Act
+                Disable-AzLogicAppsFromConfig -DeployFileName "$PSScriptRoot\Files\deploy-orderControl-unknownStopType.json" -ResourceGroupName "ignored-resource-group"
+
+                # Assert
+                Assert-MockCalled Get-AzLogicAppRunHistory -Times 0
+                Assert-MockCalled Get-AzLogicApp -Times 0
+            }
+            It "Doesn't disable anything when both checkType & stopType is 'None'" {
+                # Arrange
+                Mock Get-AzLogicAppRunHistory {}
+                Mock Set-AzLogicApp {}
+
+                # Act
+                Disable-AzLogicAppsFromConfig -DeployFileName "$PSScriptRoot\Files\deploy-orderControl-none.json" -ResourceGroupName "ignored-resource-group"
+
+                # Assert
+                Assert-MockCalled Get-AzLogicAppRunHistory -Times 0
+                Assert-MockCalled Set-AzLogicApp -Times 0
+            }
+            It "Disable logic app when stopType = Immediate" {
+                # Arrange
+               $resourceGroupName = "my-resource-group"
+                Mock Get-AzLogicAppRunHistory {}
+                Mock Set-AzLogicApp { }
+
+                # Act
+                Disable-AzLogicAppsFromConfig -DeployFileName "$PSScriptRoot\Files\deploy-orderControl-immediateWithoutCheck.json" -ResourceGroupName $resourceGroupName
+
+                # Assert
+                Assert-MockCalled Get-AzLogicAppRunHistory -Times 0
+                Assert-MockCalled Set-AzLogicApp -Times 1  -ParameterFilter { $ResourceGroupName -eq $resourceGroupName -and $State -eq "Disabled" -and $Name -eq "snd-async" }
+            }
+            It "Doesn't disable anything when checkType = NoWaitingOrRunningRuns but returns a zero count on the running runs for unknown stopType" {
+                # Arrange
+                $resourceGroupName = "my-resource-group"
+                Mock Get-AzLogicAppRunHistory { 
+                    return @([pscustomobject]@{ Status = "Waiting" })
+                }
+                Mock Set-AzLogicApp { }
+
+                # Act
+                Disable-AzLogicAppsFromConfig -DeployFileName "$PSScriptRoot\Files\deploy-orderControl-noWaitingOrRunningRunsWithunknownStopType.json" -ResourceGroupName $resourceGroupName
+
+                # Assert
+                Assert-MockCalled Get-AzLogicAppRunHistory -Times 6 -ParameterFilter { $ResourceGroupName -eq $resourceGroupName }
+                Assert-MockCalled Set-AzLogicApp -Times 0
+            }
+            It "Doesn't disable anything when checkType = NoWaitingOrRunningRuns but returns a zero count on an the waiting runs for stopType = None" {
+                # Arrange
+                $resourceGroupName = "my-resource-group"
+                Mock Get-AzLogicAppRunHistory {
+                    return @([pscustomobject]{ Status = "Running" })
+                }
+                Mock Set-AzLogicApp { }
+
+                # Act
+                Disable-AzLogicAppsFromConfig -DeployFileName "$PSScriptRoot\Files\deploy-orderControl-noWaitingOrRunningRunsWithNoneStopType.json" -ResourceGroupName $resourceGroupName
+
+                # Assert
+                Assert-MockCalled Get-AzLogicAppRunHistory -Times 4 -ParameterFilter { $ResourceGroupName -eq $resourceGroupName }
+                Assert-MockCalled Set-AzLogicApp -Times 0
+            }
+            It "Disbales all logic apps when checkType = NoWaitingOrRunningRuns with found waiting and no running runs for stopType = Immediate" {
+                # Arrange
+                $resourceGroupName = "my-resource-group"
+                Mock Get-AzLogicAppRunHistory { return @([pscustomobject]@{ Status = "Waiting" }) }
+                Mock Set-AzLogicApp { }
+
+                # Act
+                Disable-AzLogicAppsFromConfig -DeployFileName "$PSScriptRoot\Files\deploy-orderControl-noWaitingOrRunningRunsWithImmediate.json" -ResourceGroupName $resourceGroupName
+
+                # Assert
+                Assert-MockCalled Get-AzLogicAppRunHistory -Times 10 -ParameterFilter { $ResourceGroupName -eq $resourceGroupName }
+                Assert-MockCalled Set-AzLogicApp -Times 5 -ParameterFilter { $ResourceGroupName -eq $resourceGroupName }
+            }
+            It "Disables all logic apps when checkType = NoWaitingOrRunningRuns with found waiting and running runs for stopType = Immediate" {
+                # Arrange
+                $resourceGroupName = "my-resource-group"
+                $i = 0
+                Mock Get-AzLogicAppRunHistory {
+                    $script:i++
+                    if ($script:i -gt 2) {
+                        Write-Host "Returning empty (no running, no waiting) runs"
+                        return @()
+                    } else {
+                        Write-Host "Returning 1 running & 1 waiting runs"
+                        return @(
+                            [pscustomobject]@{ Status = "Running" },
+                            [pscustomobject]@{ Status = "Waiting" }
+                        )
+                    }
+                }
+                Mock Set-AzLogicApp { }
+
+                # Act
+                Disable-AzLogicAppsFromConfig -DeployFileName "$PSScriptRoot\Files\deploy-orderControl-noWaitingOrRunningRunsWithSingleImmediate.json" -ResourceGroupName $resourceGroupName
+
+                # Assert
+                Assert-MockCalled Get-AzLogicAppRunHistory -Times 4 -ParameterFilter { $ResourceGroupName -eq $resourceGroupName }
+                Assert-MockCalled Set-AzLogicApp -Times 1 -ParameterFilter { $ResourceGroupName -eq $resourceGroupName }
+            }
+        }
+    }
+}
