c5d264717a5b5aeb535a899b33744155de89faea
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2020-10-23 14:47:29 +0200

    Feature - remove all resource group locks

--- src/Arcus.Scripting.ARM/Arcus.Scripting.ARM.psm1
+++ src/Arcus.Scripting.ARM/Arcus.Scripting.ARM.psm1
@@ -12,10 +12,34 @@
   The file path to the ARM template to inject the external files into.
 #>
 function Inject-ArmContent {
-	param (
-		[string] $Path = $PSScriptRoot
-	)
-	. $PSScriptRoot\Scripts\Inject-ArmContent.ps1 -Path $Path
+    param (
+        [string] $Path = $PSScriptRoot
+    )
+    . $PSScriptRoot\Scripts\Inject-ArmContent.ps1 -Path $Path
 }
 
 Export-ModuleMember -Function Inject-ArmContent
+
+<#
+ .Synopsis
+  Remove locks on a resource group
+
+ .Description
+  In some deployments, resource-locks are assigned. This function removes all these locks.
+
+ .Parameter ResourceGroupnName
+  The name of the resource group where the locks should be removed.
+
+ .Parameter LockName
+  The optional name of the lock to remove. When this is not provided, all the locks will be removed.
+#>
+function Remove-AzResourceGroupLocks {
+    param(
+        [Parameter(Mandatory=$true)][string]$ResourceGroupName = $(throw "ResourceGroup is required"),
+        [Parameter(Mandatory=$false)][string]$LockName
+    )
+
+    . $PSScriptRoot\Scripts\Remove-AzResourceGroupLocks.ps1 -ResourceGroupName $ResourceGroupName -LockName $LockName
+}
+
+Export-ModuleMember -Function Remove-AzResourceGroupLocks
---
+++ src/Arcus.Scripting.ARM/Scripts/Remove-AzResourceGroupLocks.ps1
@@ -0,0 +1,27 @@
+param (
+    [Parameter(Mandatory=$true)][string]$ResourceGroupName = $(throw "ResourceGroup is required"),
+    [Parameter(Mandatory=$false)][string]$LockName
+)
+
+$locks = $null
+
+if ($LockName -eq $null) {
+    Write-Host "Retrieving all locks in resourceGroup '$ResourceGroupName'"
+    $locks = Get-AzResourceLock -ResourceGroupName $ResourceGroupName
+} else {
+    Write-Host "Retrieving all locks in resourceGroup '$ResourceGroupName' with name '$LockName'"
+    $locks = Get-AzResourceLock -LockName $LockName -ResourceGroupName $ResourceGroupName
+}
+
+if ($locks -ne $null -and $locks.Count -gt 0) {
+    Write-Host "Start removing all locks in resourceGroup '$ResourceGroupName'"
+    foreach ($lock in $locks) {
+        $lockId = $lock.LockId
+        Write-Host "Removing the lock with ID:" $lockId
+        Remove-AzResourceLock -LockId $lockId -Force
+    }
+
+    Write-Host "All locks in resourceGroup '$ResourceGroupName' have been removed"
+} else {
+    Write-Host "No locks to remove."
+}
