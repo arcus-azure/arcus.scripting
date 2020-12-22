1fc4849cdb4692bc938bee681c1d985ded71fc85
Maxim Braekman <maxim.braekman@hotmail.com>
2020-12-22 13:48:42 +0100

    Feature - Enable Logic Apps by config (#102)
    * Initial commit on enable logic apps by config.
    Related to #7
    
    * Include tests for enabling Logic Apps.
    Related to #7

--- src/Arcus.Scripting.LogicApps/Arcus.Scripting.LogicApps.psm1
+++ src/Arcus.Scripting.LogicApps/Arcus.Scripting.LogicApps.psm1
@@ -21,3 +21,27 @@ function Disable-AzLogicAppsFromConfig {
 }
 
 Export-ModuleMember -Function Disable-AzLogicAppsFromConfig
+
+<#
+ .Synopsis
+  Enable all specified Logic Apps described in the order control JSON file.
+  
+ .Description
+  Enables all specified Logic Apps in a specific order. The Logic Apps to be enabled and the order in which this will be done, will be defined in the configuration file (e.g. deploy-orderControl.json).
+
+ .Parameter ResourceGroupName
+  The resource group containing the Azure Logic Apps.
+  
+ .Parameter DeployFileName
+  If your solution consists of multiple interfaces, you can specify the flow-specific name of the configuration file, if not, the script will look for a file named 'deploy-orderControl.json' by default.
+#>
+function Enable-AzLogicAppsFromConfig {
+    param(
+        [string][Parameter(Mandatory = $true)] $ResourceGroupName,
+        [string] $DeployFileName = "deploy-orderControl.json"
+    )
+    
+    . $PSScriptRoot\Scripts\Enable-AzLogicAppsFromConfig.ps1 -ResourceGroupName $ResourceGroupName -DeployFileName $DeployFileName
+}
+
+Export-ModuleMember -Function Enable-AzLogicAppsFromConfig
