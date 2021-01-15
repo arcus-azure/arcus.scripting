d176759dc42acd7a67e08dbeda815d68e80f99f1
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2021-01-15 13:39:10 +0100

    Update src/Arcus.Scripting.SQL/Scripts/RunDatabaseScript.ps1
    Co-authored-by: Maxim Braekman <maxim.braekman@hotmail.com>

--- src/Arcus.Scripting.SQL/Scripts/RunDatabaseScript.ps1
+++ src/Arcus.Scripting.SQL/Scripts/RunDatabaseScript.ps1
@@ -69,7 +69,7 @@ $selectDbVersionScript = 'SELECT TOP 1 CurrentVersionNumber FROM DatabaseVersion
 #Create a DatabaseVersion table if it doesn't exist
 if(!$tables.ItemArray.Contains('DatabaseVersion'))
 {
-    Write-Host 'DatabaseVersion does not exist yet in this database.'
+    Write-Host "The table 'DatabaseVersion' does not exist yet in this database."
     Write-Host 'Creating DatabaseVersion Table...'
     $createDbQuery = GetScriptFileText $scriptpath 'CreateDatabaseVersionTable'
     QueryDatabase $params $createDbQuery
