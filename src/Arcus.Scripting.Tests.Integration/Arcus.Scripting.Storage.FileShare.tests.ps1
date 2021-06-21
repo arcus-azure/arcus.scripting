ddf6b5e1a59c9b30211a9e31cf920d98c5f10c80
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2021-06-21 12:31:34 +0200

    test: add az fileshare storage integration tests with real az resources

---
+++ src/Arcus.Scripting.Tests.Integration/Arcus.Scripting.Storage.FileShare.tests.ps1
@@ -0,0 +1,47 @@
+Import-Module Az.Storage
+Import-Module -Name $PSScriptRoot\..\Arcus.Scripting.Storage.FileShare -ErrorAction Stop
+
+InModuleScope Arcus.Scripting.Storage.FileShare {
+    Describe "Arcus Azure FileShare storage integration tests" {
+        BeforeEach {
+            $config = & $PSScriptRoot\Load-JsonAppsettings.ps1 -fileName "appsettings.json"
+            & $PSScriptRoot\Connect-AzAccountFromConfig.ps1 -config $config
+
+            $fileShareName = "arcus-scripting-storage-fileshare"
+            $storageAccount = Get-AzStorageAccount -ResourceGroupName $config.Arcus.ResourceGroupName -Name $config.Arcus.Storage.StorageAccount.Name
+            New-AzStorageShare `
+                -Context $storageAccount.Context `
+                -ResourceGroupName $config.Arcus.ResourceGroupName `
+                -StorageAccountName $config.Arcus.Storage.StorageAccount.Name `
+                -Name $fileShareName `
+                -AccessTier Hot `
+                -QuotaGiB 1024
+        }
+        Context "Create Azure FileShare storage folder" {
+            It "Create a new Azure FileShare storage folder" {
+                try {
+                    # Arrange
+                    $folderName = "new-arcus-fileshare-folder"
+
+                    # Act
+                    Create-AzFileShareStorageFolder `
+                        -ResourceGroupName $config.Arcus.ResourceGroupName `
+                        -StorageAccountName $config.Arcus.Storage.StorageAccount.Name `
+                        -FileShareName $fileShareName `
+                        -FolderName $folderName
+
+                    # Assert
+                    Get-AzStorageFile -ShareName $fileShareName |
+                        % { $_.GetType().Name } | 
+                        Should -Contain $folderName
+                    
+                } catch {
+                    Remove-AzStorageDirectory -Context $storageAccount.Context -ShareName $fileShareName -Path $folderName
+                }
+            }
+        }
+        AfterEach {
+            Remove-AzStorageShare -Name $fileShareName -Context $storageAccount.Context -IncludeAllSnapshot -Force
+        }
+    }
+}
