2799c64291f901c30bfc578dff5c01e388af4ffb
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2021-06-21 14:08:32 +0200

    pr-fix: use guid to generate unique file shares

--- src/Arcus.Scripting.Tests.Integration/Arcus.Scripting.Storage.FileShare.tests.ps1
+++ src/Arcus.Scripting.Tests.Integration/Arcus.Scripting.Storage.FileShare.tests.ps1
@@ -7,9 +7,9 @@ InModuleScope Arcus.Scripting.Storage.FileShare {
             $config = & $PSScriptRoot\Load-JsonAppsettings.ps1 -fileName "appsettings.json"
             & $PSScriptRoot\Connect-AzAccountFromConfig.ps1 -config $config
 
-            $fileShareName = "arcus-scripting-storage-fileshare"
+            $fileShareName = "arcus-scripting-storage-fileshare-$([System.Guid]::NewGuid())"
             $storageAccount = Get-AzStorageAccount -ResourceGroupName $config.Arcus.ResourceGroupName -Name $config.Arcus.Storage.StorageAccount.Name
-            New-AzStorageShare -Context $storageAccount.Context -Name $fileShareName `
+            New-AzStorageShare -Context $storageAccount.Context -Name $fileShareName
         }
         Context "Create Azure FileShare storage folder" {
             It "Creates a new Azure FileShare storage folder" {
