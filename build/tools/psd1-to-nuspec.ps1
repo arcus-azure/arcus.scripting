4237adab8bd3cd6e3983bab68592b8b8effea5bc
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2021-07-16 13:51:47 +0200

    Update psd1-to-nuspec.ps1

--- build/tools/psd1-to-nuspec.ps1
+++ build/tools/psd1-to-nuspec.ps1
@@ -383,15 +383,15 @@ function New-NuSpecFile
   }
  
   # Toggled out - not needed
-  #$dscResourceNames = Get-ExportedDscResources -PSModuleInfo $PSModuleInfo
-  #if($dscResourceNames)
-  #{
-  #  $Tags += 'PSIncludes_DscResource'
+  $dscResourceNames = Get-ExportedDscResources -PSModuleInfo $PSModuleInfo
+  if($dscResourceNames)
+  {
+    $Tags += 'PSIncludes_DscResource'
 
-  #  $Tags += $dscResourceNames | Microsoft.PowerShell.Core\ForEach-Object {
-  #    "PSDscResource_$_"
-  #  }
-  #}
+    $Tags += $dscResourceNames | Microsoft.PowerShell.Core\ForEach-Object {
+      "PSDscResource_$_"
+    }
+  }
 
   $RoleCapabilityNames = Get-AvailableRoleCapabilityName -PSModuleInfo $PSModuleInfo
   if($RoleCapabilityNames)
