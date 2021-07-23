4aebf0d8ac3634b384070aa97a3444d9978075d7
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2021-07-23 12:00:37 +0200

    test: add integration test for saving az devops build forever

---
+++ src/Arcus.Scripting.Tests.Integration/Arcus.Scripting.DevOps.tests.ps1
@@ -0,0 +1,37 @@
+Import-Module -Name $PSScriptRoot\..\Arcus.Scripting.DevOps -ErrorAction Stop
+
+InModuleScope Arcus.Scripting.DevOps {
+    Describe "Arcus Azure DevOps integration tests" {
+        BeforeEach {
+            $filePath = "$PSScriptRoot\appsettings.json"
+            [string]$appsettings = Get-Content $filePath
+            $config = ConvertFrom-Json $appsettings
+            
+            & $PSScriptRoot\Connect-AzAccountFromConfig.ps1 -config $config
+        }
+        Context "Save Azure DevOps build" {
+            It "Saves the Azure DevOps build indefinetely" {
+                # Arrange
+                $projectId = $env:SYSTEM_TEAMPROJECTID
+                $buildId = $env:BUILD_BUILDID
+                $collectionUri = $env:SYSTEM_COLLECTIONURI
+                $requestUri = "$collectionUri" + "$projectId/_apis/build/builds/" + $buildId + "?api-version=6.0"
+                $headers = @{ Authorization = "Bearer $env:SYSTEM_ACCESSTOKEN" }
+                try {
+                    # Act
+                    Save-AzDevOpsBuild -ProjectId $projectId -BuildId $buildId
+
+                    # Assert
+                    $getResponse = Invoke-WebRequest -Uri $requestUri -Method Get -Headers $headers
+                    $json = ConvertFrom-Json $getResponse.Content
+                    $json.keepForever | Should -Be $true
+                } finally {
+                    $retentionPayload = @{ keepforever='false' }
+                    $requestBody = $retentionPayload | ConvertTo-Json -Compress
+                    $patchResponse = Invoke-WebRequest -Uri $requestUri -Method Patch -Headers $headers -Body $requestBody -ContentType "application/json"
+                    $patchResponse.StatusCode | Should -Be 200
+                }
+            }
+        }
+    }
+}
