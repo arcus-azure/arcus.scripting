20895b2dbf7f84a0c91fad3bc7e619ac4222070f
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2021-08-10 09:37:38 +0200

    test: add integration test for saving az devops build forever (#202)
    * test: add integration test for saving az devops build forever
    
    * pr-temp: verify access token filled out
    
    * pr-fix: pass along access token
    
    * pr-fix: correct pipe character
    
    * Update Arcus.Scripting.DevOps.tests.ps1

--- src/Arcus.Scripting.DevOps/Scripts/Save-AzDevOpsBuild.ps1
+++ src/Arcus.Scripting.DevOps/Scripts/Save-AzDevOpsBuild.ps1
@@ -1,28 +1,26 @@
 param(
    [Parameter(Mandatory = $true)][string] $ProjectId = $(throw "ProjectId is required"),
-   [Parameter(Mandatory = $true)][string] $BuildId = $(throw "BuildId is required")   
+   [Parameter(Mandatory = $true)][string] $BuildId = $(throw "BuildId is required")
 )
 
-$retentionPayload = @{        
+$retentionPayload = @{
   keepforever='true'
 }
 
 $requestBody = $retentionPayload | ConvertTo-Json -Depth 1 -Compress
 
 $collectionUri = $env:SYSTEM_COLLECTIONURI
-
-if( $collectionUri.EndsWith('/') -eq $False ){
+if ($collectionUri.EndsWith('/') -eq $false) {
   $collectionUri = $collectionUri + '/'
 }
 
 $requestUri = "$collectionUri" + "$ProjectId/_apis/build/builds/" + $BuildId + "?api-version=6.0"
 
-Write-Verbose "Saving AzDevOps build with buildid $BuildId in project $ProjectId by posting $requestBody to $requestUri"
+Write-Verbose "Saving Azure DevOps build with build ID $BuildId in project $ProjectId by posting $requestBody to $requestUri"
+$response = Invoke-WebRequest -Uri $requestUri -Method Patch -Body $requestBody -ContentType "application/json" -Headers @{ Authorization = "Bearer $env:SYSTEM_ACCESSTOKEN" }
 
-$response = Invoke-WebRequest -Uri $requestUri -Method Patch -Body $requestBody -ContentType "application/json" -Headers @{ Authorization = "Bearer $env:SYSTEM_ACCESSTOKEN" }      
-
-if( $response.StatusCode -ne 200 ) {
-    Throw "Unable to retain build indefinetely. API request returned statuscode $($response.StatusCode)"    
+if ($response.StatusCode -ne 200) {
+    throw "Unable to retain build indefinetely. API request returned statuscode $($response.StatusCode)"
 }
 
-exit 0
+Write-Host "Azure DevOps build with build ID $BuildId in project $ProjectId saved"
