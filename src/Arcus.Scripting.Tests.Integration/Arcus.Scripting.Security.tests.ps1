dea442392d3f30a8b4e3f67003798e00811f62ba
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2021-08-16 14:36:35 +0200

    pr-fix: make sure we correctly use the user-configured lock name

--- src/Arcus.Scripting.Tests.Integration/Arcus.Scripting.Security.tests.ps1
+++ src/Arcus.Scripting.Tests.Integration/Arcus.Scripting.Security.tests.ps1
@@ -3,29 +3,75 @@ Import-Module -Name $PSScriptRoot\..\Arcus.Scripting.Security -ErrorAction Stop
 InModuleScope Arcus.Scripting.Security {
     Describe "Arcus Azure security integration tests" {
         BeforeEach {
-            $filePath = "$PSScriptRoot\appsettings.json"
+            $filePath = "$PSScriptRoot\appsettings.local.json"
             [string]$appsettings = Get-Content $filePath
             $config = ConvertFrom-Json $appsettings
             
             & $PSScriptRoot\Connect-AzAccountFromConfig.ps1 -config $config
         }
         Context "Removing resource locks on Azure resources" {
-            It "Newly added resource lock gets removed by removing all resource locks" {
+            It "Newly added resource lock gets removed by removing all resource locks with a given lock name" {
                 # Arrange
-                $lockName = "NewTestingLock"
+                $lockName = "NewTestingLockWithName"
                 $targetResourceName = $config.Arcus.KeyVault.VaultName
                 $targetResourceGroupName = $config.Arcus.ResourceGroupName
-                NewAzResourceLock -LockName $lockName -ResouceGroupName $targetResourceGroupName -ResourceName $targetResourceName
+                New-AzResourceLock `
+                    -ResourceGroupName $targetResourceGroupName `
+                    -ResourceName $targetResourceName `
+                    -ResourceType "Microsoft.KeyVault/vaults" `
+                    -LockName $lockName `
+                    -LockLevel CanNotDelete `
+                    -Force
 
                 try {
                     # Act
                     Remove-AzResourceGroupLocks -ResourceGroupName $targetResourceGroupName -LockName $lockName
                 
                     # Assert
-                    $locks = Get-AzResourceLock -LockName $lockName -ResourceGroupName $targetResourceGroupName
-                    $locks.Count | Should -Be 0
+                    $locks = Get-AzResourceLock -ResourceGroupName $targetResourceGroupName
+                    $locks.Name | Should -Not -BeIn $lockName
                 } finally {
-                    Remove-AzResourceLock -LockName $lockName -ResourceName $targetResourceName -ResourceGroupName $targetResourceGroupName
+                    $locks = Get-AzResourceLock -ResourceGroupName $targetResourceGroupName
+                    if ($lockName -in $locks.Name) {
+                        Remove-AzResourceLock `
+                            -ResourceGroupName $targetResourceGroupName `
+                            -LockName $lockName `
+                            -ResourceName $targetResourceName `
+                            -ResourceType "Microsoft.KeyVault/vaults" `
+                            -Force
+                    }
+                }
+            }
+            It "Newly added resource lock gets removed by removing all resource locks without giving any lock name" {
+                # Arrange
+                $lockName = "NewTestingLockWithoutName"
+                $targetResourceName = $config.Arcus.KeyVault.VaultName
+                $targetResourceGroupName = $config.Arcus.ResourceGroupName
+                New-AzResourceLock `
+                    -ResourceGroupName $targetResourceGroupName `
+                    -ResourceName $targetResourceName `
+                    -ResourceType "Microsoft.KeyVault/vaults" `
+                    -LockName $lockName `
+                    -LockLevel CanNotDelete `
+                    -Force
+
+                try {
+                    # Act
+                    Remove-AzResourceGroupLocks -ResourceGroupName $targetResourceGroupName
+                
+                    # Assert
+                    $locks = Get-AzResourceLock -ResourceGroupName $targetResourceGroupName
+                    $locks.Name | Should -Not -BeIn $lockName
+                } finally {
+                    $locks = Get-AzResourceLock -ResourceGroupName $targetResourceGroupName
+                    if ($lockName -in $locks.Name) {
+                        Remove-AzResourceLock `
+                            -ResourceGroupName $targetResourceGroupName `
+                            -LockName $lockName `
+                            -ResourceName $targetResourceName `
+                            -ResourceType "Microsoft.KeyVault/vaults" `
+                            -Force
+                    }
                 }
             }
         }
