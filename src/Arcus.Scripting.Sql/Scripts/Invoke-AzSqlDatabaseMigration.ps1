8b946ba5d336bce0d1d91732afa8f687114ae21f
Frederik Gheysels <frederik.gheysels@telenet.be>
2021-08-20 17:28:53 +0200

    Changes to support Semantic Versioning on DB changes

--- src/Arcus.Scripting.Sql/Scripts/Invoke-AzSqlDatabaseMigration.ps1
+++ src/Arcus.Scripting.Sql/Scripts/Invoke-AzSqlDatabaseMigration.ps1
@@ -1,25 +1,36 @@
 param(
     [Parameter(Mandatory=$true)][string] $ServerName = $(throw "Please provide the name of the SQL Server that hosts the SQL Database. (Do not include 'database.windows.net'"),
     [Parameter(Mandatory=$true)][string] $DatabaseName = $(throw "Please provide the name of the SQL Database"),
-    [Parameter(Mandatory=$true)][string] $UserName = $(throw "Please provide the UserName of the SQL Database"),
-    [Parameter(Mandatory=$true)][string] $Password = $(throw "Please provide the Password of the SQL Database"),
+    [Parameter(Mandatory=$true)][string] $UserName = $(throw "Please provide the UserName of the User that must be used to perform the update"),
+    [Parameter(Mandatory=$true)][string] $Password = $(throw "Please provide the Password of the User that must be used to perform the update"),
     [Parameter(Mandatory=$false)][string] $ScriptsFolder = "$PSScriptRoot/sqlScripts",
     [Parameter(Mandatory=$false)][string] $ScriptsFileFilter = "*.sql",
     [Parameter(Mandatory=$false)][string] $DatabaseSchema = "dbo"
 )
+
+#Import needed for Azure Powershell Release Step
+Import-Module SqlServer
 
 Write-Host "Looking for SQL scripts in folder: $ScriptsFolder"
 
 #Functions for repeated use
 function Execute-DbCommand($params, [string]$query)
 {
-    $result = Invoke-Sqlcmd @params -Query $query -Verbose -QueryTimeout 180
-    Write-Host "Result from querying $result"
+    $result = Invoke-Sqlcmd @params -Query $query -Verbose -QueryTimeout 180 -ErrorAction Stop -ErrorVariable err
+    
+    if( $err )
+    {
+        throw ($err)
+    }
 }
 
 function Execute-DbCommandWithResult($params, [string] $query)
 {
-    $result = Invoke-Sqlcmd @params -Query $query -Verbose
+    $result = Invoke-Sqlcmd @params -Query $query -Verbose -ErrorAction Stop -ErrorVariable err
+    if( $err )
+    {
+        throw ($err)
+    }
     return $result
 }
 
@@ -31,10 +42,10 @@ function Create-DbParams([string] $DatabaseName, [string] $serverInstance, [stri
     
     return $params = @{
       'Database' = $DatabaseName
-      'ServerInstance' = "$serverInstance.database.windows.net"
+      'ServerInstance' = $serverInstance
       'Username' = $UserName
       'Password' = $Password
-      'OutputSqlErrors' = $true
+      'OutputSqlErrors' = $True
       'AbortOnError' = $True
     }
 }
@@ -45,110 +56,149 @@ function Get-SqlScriptFileText([string] $scriptPath, [string] $fileName)
     return $query = Get-Content $currentfilepath
 }
 
-function Get-CurrentDbVersionNumber () {
-    $selectDbVersionScript = "SELECT TOP 1 CurrentVersionNumber FROM [$DatabaseSchema].[DatabaseVersion] ORDER BY CurrentVersionNumber DESC"
-    $params = Create-DbParams $DatabaseName $ServerName $UserName $Password
+Class DatabaseVersion : System.IComparable
+{
+    [int] $MajorVersionNumber
+    [int] $MinorVersionNumber
+    [int] $PatchVersionNumber
 
-     Write-Host 'Getting Current Database Version Number...'
-    $databaseVersionNumberDataRow = Execute-DbCommandWithResult $params $selectDbVersionScript
-
-     if ($databaseVersionNumberDataRow.ItemArray.Count -eq 0)
+    DatabaseVersion([int] $major, [int] $minor, [int] $patch)
     {
-        $databaseVersionNumber = 0;
-    }
-    else
-    {
-        $databaseVersionNumber = [Convert]::ToInt32($databaseVersionNumberDataRow.ItemArray[0])
+        $this.MajorVersionNumber = $major;
+        $this.MinorVersionNumber = $minor;
+        $this.PatchVersionNumber = $patch;
     }
 
-    return $databaseVersionNumber
+    DatabaseVersion([string] $version)
+    {
+        $items = $version -split '\.'
+
+        $this.MajorVersionNumber = $items[0];
+        $this.MinorVersionNumber = $items[1];
+        $this.PatchVersionNumber = $items[2];
+    }
+
+    DatabaseVersion()  
+    {
+        $this.MajorVersionNumber = 0;
+        $this.MinorVersionNumber = 0;
+        $this.PatchVersionNumber = 0;
+    }
+
+    [int] CompareTo($other)
+    {
+        $result = $this.MajorVersionNumber.CompareTo($other.MajorVersionNumber)
+
+        if( $result -eq 0 )
+        {
+            $result = $this.MinorVersionNumber.CompareTo($other.MinorVersionNumber)
+
+            if( $result -eq 0 )
+            {
+                return $this.PatchVersionNumber.CompareTo($other.PatchVersionNumber)
+            }            
+        }
+
+        return $result;                
+    }
+
+    [bool] Equals($other)
+    {
+        return $this.MajorVersionNumber -eq $other.MajorVersionNumber -and $this.MinorVersionNumber -eq $other.MinorVersionNumber -and $this.PatchVersionNumber -eq $other.PatchVersionNumber
+    }
+
+    [string] ToString()
+    {
+        return $this.MajorVersionNumber.ToString() + "." + $this.MinorVersionNumber.ToString() + "." + $this.PatchVersionNumber.ToString()
+    }
 }
 
 #Group params needed to connect to database for ease of use
 $params = Create-DbParams $DatabaseName $ServerName $UserName $Password
 
-#Get all tables on the database
-$query = 'SELECT TABLE_NAME FROM information_schema.tables'
-$tables = Execute-DbCommandWithResult $params $query
+# Create the DatabaseVersion table if it doesn't exist yet
+$createDatabaseVersionTable = "IF NOT EXISTS ( SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'DatabaseVersion' AND TABLE_SCHEMA = '$DatabaseSchema' ) " +
+                              "BEGIN " +
+	                          "CREATE TABLE [$DatabaseSchema].[DatabaseVersion] " +
+	                          "( " +
+	                          "   [MajorVersionNumber] INT NOT NULL, " +
+		                      "   [MinorVersionNumber] INT NOT NULL, " +
+		                      "   [PatchVersionNumber] INT NOT NULL, " +
+		                      "   [MigrationDescription] [nvarchar](256) NOT NULL, " +
+                              "   [MigrationDate] DATETIME NOT NULL " +
+		                      "   CONSTRAINT [PK_DatabaseVersion] PRIMARY KEY CLUSTERED  ([MajorVersionNumber],[MinorVersionNumber],[PatchVersionNumber]) " +
+                              "				WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) " +
+	                          ") " +
+                              "END "
 
-# Get script to determine DB version
-$selectDbVersionScript = "SELECT TOP 1 CurrentVersionNumber FROM [$DatabaseSchema].[DatabaseVersion] ORDER BY CurrentVersionNumber DESC"
+Execute-DbCommand $params $createDatabaseVersionTable
 
-#Create a DatabaseVersion table if it doesn't exist
-if(!$tables.ItemArray.Contains('DatabaseVersion'))
+$getCurrentDbVersionQuery = "SELECT TOP 1 MajorVersionNumber, MinorVersionNumber, PatchVersionNumber FROM DatabaseVersion ORDER BY MajorVersionNumber DESC, MinorVersionNumber DESC, PatchVersionNumber DESC"
+
+$databaseVersionNumberDataRow = Execute-DbCommandWithResult $params $getCurrentDbVersionQuery
+
+$databaseVersion = [DatabaseVersion]::new()
+
+if( !($null -eq $databaseVersionNumberDataRow ) )
 {
-    Write-Host "The table 'DatabaseVersion' does not exist yet in this database."
-    Write-Host 'Creating DatabaseVersion Table...'
-    $createDbCommand = Get-SqlScriptFileText $ScriptsFolder 'CreateDatabaseVersionTable'
-    Execute-DbCommand $params $createDbCommand
-    Write-Host 'DatabaseVersion Table Created'
-
-    $databaseVersionNumber = 0
-}
-else
-{
-    # Get database versionnumber
-    $databaseVersionNumber = Get-CurrentDbVersionNumber
+    $databaseVersion = [DatabaseVersion]::new([convert]::ToInt32($databaseVersionNumberDataRow.ItemArray[0]), [convert]::ToInt32($databaseVersionNumberDataRow.ItemArray[1]), [convert]::ToInt32($databaseVersionNumberDataRow.ItemArray[2]))    
 }
 
+Write-Host "Current database-version number: " $databaseVersion
 
-Write-Host "Current databaseVersionNumber : $databaseVersionNumber"
+$files = Get-ChildItem -Path $ScriptsFolder -Filter $ScriptsFileFilter | Sort-Object {($_.BaseName -split '_')[0] -as [DatabaseVersion]}
 
-#Run all necessary scripts
-$files = Get-ChildItem -Path $ScriptsFolder -Filter $ScriptsFileFilter | Sort-Object {$_.BaseName -replace "\D+" -as [Int]}
-
-for ($i=0; $i -lt $files.Count; $i++) 
+# Execute each migration file who's versionnumber is higher then the current DB version
+for ($i = 0; $i -lt $files.Count; $i++) 
 {
     $fileName = $files[$i].BaseName
-    $fileNameSections = $fileName.Split('_');
 
-    if ($fileNameSections.Length -lt 2)
+    $fileNameParts = $fileName.Split('_')
+
+    if ($fileNameParts.Length -lt 2)
     {
         Write-Host "File $fileName skipped for not having all required name sections (version and description)."
         continue;
     }
 
-    $fileVersionStr = $fileNameSections[1];
-
-    [int] $scriptVersionNumber = -1;
-
-    if (-Not([int32]::TryParse($fileVersionStr, [ref]$scriptVersionNumber)))
+    if ($fileNameParts[0] -match "\d.\d.\d" -eq $False )
     {
         Write-Host "File $fileName skipped because version is not valid."
         continue;
     }
 
-    [string] $scriptVersionDescription = [convert]::Tostring($fileNameSections[2])
-    if($scriptVersionDescription.Length -gt 256)
+    [DatabaseVersion] $scriptVersionNumber = [DatabaseVersion]::new($fileNameParts[0])
+    [string] $migrationDescription = $fileNameParts[1]
+
+    if( $scriptVersionNumber -le $databaseVersion )
     {
-        Write-Host "Need to truncate the migration description because its size is" $scriptVersionDescription.Length "while the maximum size is 256"
-        $scriptVersionDescription = $scriptVersionDescription.Substring(0,255)
+        Write-Verbose "Skipped Migration $scriptVersionNumber as it has already been applied"
+        continue
     }
+
+    Write-Host "Executing DB migration " $scriptVersionNumber ": " $migrationDescription "... "
+
+    $migrationScript = [IO.File]::ReadAllText($files[$i].FullName)
+
+    Execute-DbCommand $params $migrationScript
+
+    if($migrationDescription.Length -gt 256)
+    {
+		Write-Host "Need to truncate the migration description because its size is" $scriptVersionDescription.Length "while the maximum size is 256"
+        $migrationDescription = $migrationDescription.Substring(0, 256)
+    }
+	
+    $updateVersionQuery =   "INSERT INTO [$DatabaseSchema].[DatabaseVersion] ([MajorVersionNumber], [MinorVersionNumber], [PatchVersionNumber], [MigrationDescription], [MigrationDate]) " +
+                            "SELECT $($scriptVersionNumber.MajorVersionNumber), $($scriptVersionNumber.MinorVersionNumber), $($scriptVersionNumber.PatchVersionNumber), '$migrationDescription', getdate()"
     
-    Write-Host "Found migration #$scriptVersionNumber with description '$scriptVersionDescription'"
+    Execute-DbCommand $params $updateVersionQuery
 
-    if($scriptVersionNumber -eq ($databaseVersionNumber + 1))
-    {
-        Write-Host "Running script #$scriptVersionNumber"
+    Write-Host "DB migration " $scriptVersionNumber " applied!"
 
-        # Perform migration
-        $text = Get-SqlScriptFileText $ScriptsFolder $fileName
-        Execute-DbCommand $params $text
-
-        # Append the new version and description in version table
-        $updateVersionQuery = "INSERT INTO [$DatabaseSchema].[DatabaseVersion] ([CurrentVersionNumber], [MigrationDescription]) VALUES ($scriptVersionNumber, '$scriptVersionDescription')"
-        Execute-DbCommand $params $updateVersionQuery
-
-        # Update DB version to new version
-        $databaseVersionNumber = $scriptVersionNumber
-
-        Write-Host "Migration to version $scriptVersionNumber complete."
-    }
-    else {
-        Write-Host "Migration #$scriptVersionNumber skipped"
-    }
+    $databaseVersion = $scriptVersionNumber    
 }
 
 #Get New Database Version Number
-$databaseVersionNumber = Get-CurrentDbVersionNumber
-Write-Host "Done looping over scripts. Current Database version is $databaseVersionNumber."
+$databaseVersionNumberDataRow = Execute-DbCommandWithResult $params $getCurrentDbVersionQuery  
+$databaseVersionNumber = [DatabaseVersion]::new([convert]::ToInt32($databaseVersionNumberDataRow.ItemArray[0]), [convert]::ToInt32($databaseVersionNumberDataRow.ItemArray[1]), [convert]::ToInt32($databaseVersionNumberDataRow.ItemArray[2]))    
+Write-Host "Done migrating database. Current Database version is $databaseVersionNumber."
