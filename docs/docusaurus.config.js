2748b7227c881ec2f680ce58133a31150b0a64d1
Michael Calewaert <michael.calewaert@codit.eu>
2021-10-08 16:12:32 +0200

    docs: Add search

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -12,6 +12,15 @@ module.exports = {
   organizationName: 'arcus-azure', // Usually your GitHub org/user name.
   projectName: 'Arcus - Scripting', // Usually your repo name.
   themeConfig: {
+    algolia: {
+      apiKey: 'fdbac718aeb246ac60aeae5bd286cbbf',
+      indexName: 'arcus-azure',
+      // Set `contextualSearch` to `true` when having multiple versions!!!
+      contextualSearch: true,
+      searchParameters: {
+        facetFilters: ["tags:scripting"]
+      },
+    },
     image: 'img/arcus.jpg',
     navbar: {
       title: 'Scripting',
