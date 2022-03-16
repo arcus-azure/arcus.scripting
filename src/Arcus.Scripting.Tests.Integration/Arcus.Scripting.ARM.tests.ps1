1e59af614ade0d99ff4541d6a5b4dd10d54b2aa2
Pieter Vromant <Pieter.Vromant@codit.eu>
2022-03-16 10:31:35 +0100

    adapted integration tests

--- src/Arcus.Scripting.Tests.Integration/Arcus.Scripting.ARM.tests.ps1
+++ src/Arcus.Scripting.Tests.Integration/Arcus.Scripting.ARM.tests.ps1
@@ -20,7 +20,7 @@ InModuleScope Arcus.Scripting.ARM {
                 }
             }
             if ([Environment]::OSVersion.VersionString -like "*Windows*") {
-                It "Replaces file path with file contents as JSON object (windows)" {
+                It "Replaces relative file path with file contents as JSON object (windows)" {
                     # Arrange
                     $armTemplateFile = "$PSScriptRoot\Files\arm-template-object (windows).json"
                     try {
@@ -36,8 +36,26 @@ InModuleScope Arcus.Scripting.ARM {
                         Get-Content $originalFile | Out-File -FilePath $armTemplateFile
                     }
                 }
+                It "Replaces absolute file path with file contents as JSON object (windows)" {
+                    # Arrange
+                    $armTemplateFile = "$PSScriptRoot\Files\arm-template-object-absolutepath (windows).json"
+                    $armTemplateDirectory = (Get-Item $armTemplateFile).Parent.FullName
+                    ((Get-Content -path $armTemplateFile -Raw) -replace '#{ArmTemplateDirectory}#', $armTemplateDirectory) | Set-Content -Path $armTemplateFile
+                    try {
+                        # Act
+                        Inject-ArmContent -Path $armTemplateFile
+
+                        # Assert
+                        $expected = Get-Content "$PSScriptRoot\Files\arm-template-object-value (windows).json"
+                        $actual = Get-Content $armTemplateFile
+                        $actual[7] | Should -Be '    "value": "{\r\n   \"test\": \"this is a test value\"\r\n}",'
+                    } finally {
+                        $originalFile = "$PSScriptRoot\Files\arm-template-object-absolutepath-org (windows).json"
+                        Get-Content $originalFile | Out-File -FilePath $armTemplateFile
+                    }
+                }
             } else {
-                It "Replaces file path with file contents as JSON object (linux)" {
+                It "Replaces relative file path with file contents as JSON object (linux)" {
                     # Arrange
                     $armTemplateFile = "$PSScriptRoot\Files\arm-template-object (linux).json"
                     try {
@@ -50,6 +68,24 @@ InModuleScope Arcus.Scripting.ARM {
                         $actual[7] | Should -Be '    "value": "{\n   \"test\": \"this is a test value\"\n}",'
                     } finally {
                         $originalFile = "$PSScriptRoot\Files\arm-template-object-org (linux).json"
+                        Get-Content $originalFile | Out-File -FilePath $armTemplateFile
+                    }
+                }
+                It "Replaces absolute file path with file contents as JSON object (linux)" {
+                    # Arrange
+                    $armTemplateFile = "$PSScriptRoot\Files\arm-template-object-absolutepath (linux).json"
+                    $armTemplateDirectory = (Get-Item $armTemplateFile).Parent.FullName
+                    ((Get-Content -path $armTemplateFile -Raw) -replace '#{ArmTemplateDirectory}#', $armTemplateDirectory) | Set-Content -Path $armTemplateFile
+                    try {
+                        # Act
+                        Inject-ArmContent -Path $armTemplateFile
+
+                        # Assert
+                        $expected = Get-Content "$PSScriptRoot\Files\arm-template-object-value (linux).json"
+                        $actual = Get-Content $armTemplateFile
+                        $actual[7] | Should -Be '    "value": "{\n   \"test\": \"this is a test value\"\n}",'
+                    } finally {
+                        $originalFile = "$PSScriptRoot\Files\arm-template-object-absolutepath-org (linux).json"
                         Get-Content $originalFile | Out-File -FilePath $armTemplateFile
                     }
                 }
