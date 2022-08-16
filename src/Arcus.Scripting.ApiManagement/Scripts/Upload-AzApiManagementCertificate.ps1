89aba7ce8be0d23e16ca10d84d1e9f2419ee8368
Pim Simons <pim.simons@codit.eu>
2022-08-16 14:02:32 +0200

    fix apimContext name

--- src/Arcus.Scripting.ApiManagement/Scripts/Upload-AzApiManagementCertificate.ps1
+++ src/Arcus.Scripting.ApiManagement/Scripts/Upload-AzApiManagementCertificate.ps1
@@ -12,5 +12,5 @@ if ($apimContext -eq $null) {
 Write-Host "Using API Management instance '$ServiceName' in resource group '$ResourceGroupName'"
 
 Write-Verbose "Uploading private certificate at '$CertificateFilePath'..."
-New-AzApiManagementCertificate -Context $context -PfxFilePath $CertificateFilePath -PfxPassword $CertificatePassword
+New-AzApiManagementCertificate -Context $apimContext -PfxFilePath $CertificateFilePath -PfxPassword $CertificatePassword
 Write-Host "Uploaded private certificate at '$CertificateFilePath'"
