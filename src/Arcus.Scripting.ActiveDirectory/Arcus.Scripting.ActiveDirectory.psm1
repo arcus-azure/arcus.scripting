448ec5f6e76f8519b8a1c1cc4aaa67c489dfac5e
Pim Simons <pim.simons@codit.eu>
2022-11-03 12:31:14 +0100

    added second param to check specific client

--- src/Arcus.Scripting.ActiveDirectory/Arcus.Scripting.ActiveDirectory.psm1
+++ src/Arcus.Scripting.ActiveDirectory/Arcus.Scripting.ActiveDirectory.psm1
@@ -7,12 +7,16 @@
 
  .Parameter ClientId
   The client id of the Azure Active Directory Application Registration from which the role assignments are to be retrieved.
+
+ .Parameter RolesAssignedToClientId
+  The client id of the Azure Active Directory Application Registration to which roles are assigned.
 #>
 function Get-AzADAppRoleAssignments {
     param(
-       [Parameter(Mandatory = $true)][string] $ClientId = $(throw "ClientId is required")
+       [Parameter(Mandatory = $true)][string] $ClientId = $(throw "ClientId is required"),
+       [Parameter(Mandatory = $false)][string] $RolesAssignedToClientId
     )
-    . $PSScriptRoot\Scripts\Get-AzADAppRoleAssignments.ps1 -ClientId $ClientId
+    . $PSScriptRoot\Scripts\Get-AzADAppRoleAssignments.ps1 -ClientId $ClientId -RolesAssignedToClientId $RolesAssignedToClientId
 }
 
 Export-ModuleMember -Function Get-AzADAppRoleAssignments
--- src/Arcus.Scripting.ActiveDirectory/Scripts/Get-AzADAppRoleAssignments.ps1
+++ src/Arcus.Scripting.ActiveDirectory/Scripts/Get-AzADAppRoleAssignments.ps1
@@ -1,27 +1,46 @@
 param(
-    [Parameter(Mandatory = $true)][string] $ClientId = $(throw "ClientId is required")
+    [Parameter(Mandatory = $true)][string] $ClientId = $(throw "ClientId is required"),
+    [Parameter(Mandatory = $false)][string] $RolesAssignedToClientId
 )
 
 $adApplication = Get-AzADApplication -Filter "AppId eq '$ClientId'"
+$adServicePrincipal = Get-AzADServicePrincipal -Filter "AppId eq '$ClientId'"
 if (!$adApplication) { 
     throw "Active Directory Application for the ClientId $ClientId could not be found" 
 }
-
-$adServicePrincipal = Get-AzADServicePrincipal -Filter "AppId eq '$ClientId'"
 if (!$adServicePrincipal) { 
     throw "Active Directory Service Principal for the ClientId $ClientId could not be found" 
+}
+
+if ($RolesAssignedToClientId -ne '') {
+    $adApplicationRolesAssignedTo = Get-AzADApplication -Filter "AppId eq '$RolesAssignedToClientId'"
+    $adServicePrincipalRolesAssignedTo = Get-AzADServicePrincipal -Filter "AppId eq '$RolesAssignedToClientId'"
+    if (!$adApplicationRolesAssignedTo) { 
+        throw "Active Directory Application for the ClientId $RolesAssignedToClientId could not be found" 
+    }
+    if (!$adServicePrincipalRolesAssignedTo) { 
+        throw "Active Directory Service Principal for the ClientId $RolesAssignedToClientId could not be found" 
+    }
 }
 
 try {
     foreach ($appRole in $adApplication.AppRole) {
         Write-Host "Found role '$($appRole.Value)' on Active Directory Application '$($adApplication.DisplayName)':" -ForegroundColor Green
-        $serviceAppRoleAssignments = Get-MgServicePrincipalAppRoleAssignedTo -ServicePrincipalId $adServicePrincipal.Id  | Where-Object AppRoleId -eq $appRole.Id 
+        if ($RolesAssignedToClientId -ne '') {
+            $serviceAppRoleAssignments = Get-MgServicePrincipalAppRoleAssignedTo -ServicePrincipalId $adServicePrincipal.Id  | Where-Object {($_.AppRoleId -eq $appRole.Id) -and ($_.PrincipalId -eq $adServicePrincipalRolesAssignedTo.Id)}
+        } else {
+            $serviceAppRoleAssignments = Get-MgServicePrincipalAppRoleAssignedTo -ServicePrincipalId $adServicePrincipal.Id  | Where-Object AppRoleId -eq $appRole.Id 
+        }
 
-        foreach ($serviceAppRoleAssignment in $serviceAppRoleAssignments) {
-            $servicePrincipal = Get-AzADServicePrincipal -ObjectId $serviceAppRoleAssignment.PrincipalId
-            if ($servicePrincipal -ne $null) {
-                Write-Host "Role '$($appRole.Value)' is assigned to the Active Directory Application '$($serviceAppRoleAssignment.PrincipalDisplayName)' with id '$($servicePrincipal.AppId)'" -ForegroundColor White        
+        if ($serviceAppRoleAssignments) {
+            foreach ($serviceAppRoleAssignment in $serviceAppRoleAssignments) {
+                $servicePrincipal = Get-AzADServicePrincipal -ObjectId $serviceAppRoleAssignment.PrincipalId
+                if ($servicePrincipal -ne $null) {
+                    Write-Host "Role '$($appRole.Value)' is assigned to the Active Directory Application '$($serviceAppRoleAssignment.PrincipalDisplayName)' with id '$($servicePrincipal.AppId)'" -ForegroundColor White        
+                }
             }
+        } else {
+            Write-Host "No role assignments found"
         }
     }
 } catch {
