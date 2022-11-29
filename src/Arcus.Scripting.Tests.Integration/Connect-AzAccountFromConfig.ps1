bf1271dfec996f68ca9fd63aa5781673f8ad0955
Pim Simons <32359437+pim-simons@users.noreply.github.com>
2022-11-29 14:29:23 +0100

    feat: Added scripts to manage Azure Active Directory App Role Assignments (#336)
    Co-authored-by: Stijn Moreels <9039753+stijnmoreels@users.noreply.github.com>
    Co-authored-by: Pim Simons <pim.simons@codit.eu>
    Closes https://github.com/arcus-azure/arcus.scripting/issues/330

--- src/Arcus.Scripting.Tests.Integration/Connect-AzAccountFromConfig.ps1
+++ src/Arcus.Scripting.Tests.Integration/Connect-AzAccountFromConfig.ps1
@@ -2,7 +2,7 @@ param(
     [pscustomobject]$config
 )
 
- $clientSecret = ConvertTo-SecureString $config.Arcus.ServicePrincipal.ClientSecret -AsPlainText -Force
- $pscredential = New-Object -TypeName System.Management.Automation.PSCredential($config.Arcus.ServicePrincipal.ClientId, $clientSecret)
- Disable-AzContextAutosave -Scope Process
- Connect-AzAccount -Credential $pscredential -TenantId $config.Arcus.TenantId -ServicePrincipal
+$clientSecret = ConvertTo-SecureString $config.Arcus.ServicePrincipal.ClientSecret -AsPlainText -Force
+$pscredential = New-Object -TypeName System.Management.Automation.PSCredential($config.Arcus.ServicePrincipal.ClientId, $clientSecret)
+Disable-AzContextAutosave -Scope Process
+Connect-AzAccount -Credential $pscredential -TenantId $config.Arcus.TenantId -ServicePrincipal
